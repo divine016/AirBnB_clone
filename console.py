f7e15968dffb6b08740dc790589bbba0ad48b63f
Hsppie <wambimicheal@gmail.com>
2023-02-12 15:59:52 -0800

    modified

--- console.py
+++ console.py
@@ -6,7 +6,12 @@ from models.base_model import BaseModel
 from models import storage
 import re
 import json
-
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
 
